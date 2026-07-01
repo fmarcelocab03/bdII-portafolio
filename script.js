6fa53f69eee8ca3d85620d42efcc9982c3ad29ad
fmarcelocab03 <fmarcelocab03@gmail.com>
2026-07-01 15:52:54 -0500

    Update print statement from 'Hello' to 'Goodbye'

--- script.js
+++ script.js
@@ -6,6 +6,33 @@ let esAdmin = false;
   const supabaseClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
   console.log("Supabase conectado:", supabaseClient);
   document.addEventListener("DOMContentLoaded", function () {
+    particlesJS('particles-js', {
+  "particles": {
+    "number": { "value": 50, "density": { "enable": true, "value_area": 800 } },
+    "color": { "value": "#38bdf8" },
+    "shape": { "type": "circle" },
+    "opacity": { "value": 0.3, "random": false },
+    "size": { "value": 3, "random": true },
+    "line_linked": { "enable": true, "distance": 150, "color": "#7e22ce", "opacity": 0.2, "width": 1 },
+    "move": { "enable": true, "speed": 2, "direction": "none", "random": false, "straight": false, "out_mode": "out", "bounce": false }
+  },
+  "interactivity": {
+    "detect_on": "canvas",
+    "events": {
+      "onhover": { "enable": true, "mode": "repulse" },
+      "onclick": { "enable": true, "mode": "push" },
+      "resize": true
+    },
+    "modes": {
+      "grab": { "distance": 140, "line_linked": { "opacity": 1 } },
+      "bubble": { "distance": 400, "size": 40, "duration": 2, "opacity": 8, "speed": 3 },
+      "repulse": { "distance": 100, "duration": 0.4 },
+      "push": { "particles_nb": 4 },
+      "remove": { "particles_nb": 2 }
+    }
+  },
+  "retina_detect": true
+});
     ocultarAdmin();
     const GITHUB_REPO_URL = "https://github.com/fmarcelocab03/bdII-portafolio/tree/main/archivos";
     
